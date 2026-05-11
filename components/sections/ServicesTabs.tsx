d32fd84103f4aac408dbbb0d372165e986a127db
pirajoke <thegreatgatsby456@gmail.com>
2026-05-11 16:34:43 +0800

    fix: shift services image right and down

--- components/sections/ServicesTabs.tsx
+++ components/sections/ServicesTabs.tsx
@@ -145,8 +145,8 @@ export function ServicesTabs() {
           </Link>
         </div>
 
-        <div className="lg:col-span-7 lg:-ml-4 lg:pt-14 xl:-ml-8 xl:pt-16">
-          <div className="relative aspect-[4/5] w-full overflow-hidden rounded-3xl border border-navy/10 bg-navy/5 md:aspect-[4/3] lg:sticky lg:top-44 lg:aspect-[16/10] lg:rounded-[2.5rem]">
+        <div className="lg:col-span-7 lg:ml-6 lg:pt-20 xl:ml-10 xl:pt-24">
+          <div className="relative aspect-[4/5] w-full overflow-hidden rounded-3xl border border-navy/10 bg-navy/5 md:aspect-[4/3] lg:sticky lg:top-52 lg:aspect-[16/10] lg:rounded-[2.5rem]">
             {TABS.map((tab, index) => (
               <div
                 key={tab.id}
